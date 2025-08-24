2635442492090d324dd776e10a9f7ffe9d785f4b
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-24 21:05:46 +0200

    feat: add dolphin entity class (empty)

---
+++ src/main/java/de/julianweinelt/ubm/entities/EntityDolphin.java
@@ -0,0 +1,10 @@
+package de.julianweinelt.ubm.entities;
+
+import net.minecraft.entity.passive.EntityWaterMob;
+import net.minecraft.world.World;
+
+public class EntityDolphin extends EntityWaterMob {
+    public EntityDolphin(World worldIn) {
+        super(worldIn);
+    }
+}
