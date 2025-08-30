e422411ebea0f0827ee87526f82100af9e47511b
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-30 17:25:27 +0200

    feat: add fox variant: snow fox

--- src/main/java/de/julianweinelt/ubm/entities/EntityFox.java
+++ src/main/java/de/julianweinelt/ubm/entities/EntityFox.java
@@ -2,18 +2,34 @@ package de.julianweinelt.ubm.entities;
 
 import de.julianweinelt.ubm.items.ModItems;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class EntityFox extends EntityAnimal {
+
+    private static final DataParameter<Integer> VARIANT = EntityDataManager.createKey(EntityFox.class, DataSerializers.VARINT);
+
+    private static final ResourceLocation[] TEXTURES = new ResourceLocation[] {
+            new ResourceLocation("ubm:textures/entity/fox/fox.png"),
+            new ResourceLocation("ubm:textures/entity/fox/snow_fox.png")
+    };
+
     public EntityFox(World worldIn) {
         super(worldIn);
         this.setSize(0.6F, 0.7F);
@@ -23,6 +39,7 @@ public class EntityFox extends EntityAnimal {
     @Override
     protected void entityInit() {
         super.entityInit();
+        this.dataManager.register(VARIANT, 0);
     }
 
     @Override
@@ -32,6 +49,48 @@ public class EntityFox extends EntityAnimal {
         this.tasks.addTask(0, new EntityAIPanic(this, 0.5F));
         this.tasks.addTask(0, new EntityAIFollowParent(this, 0.5F));
         this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+    }
+
+
+
+    @Override
+    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
+        super.onInitialSpawn(difficulty, livingdata);
+
+        int variant = 0;
+        if (getEntityWorld().getBiome(getPosition()).isSnowyBiome()) variant = 1;
+        this.setVariant(variant);
+        return livingdata;
+    }
+
+    public int getVariant() {
+        return this.dataManager.get(VARIANT);
+    }
+
+    public void setVariant(int variant) {
+        this.dataManager.set(VARIANT, variant);
+    }
+
+    public ResourceLocation getTexture() {
+        int variant = this.getVariant();
+        if (variant < 0 || variant >= TEXTURES.length) {
+            variant = 0;
+        }
+        return TEXTURES[variant];
+    }
+
+    @Override
+    public void writeEntityToNBT(NBTTagCompound compound) {
+        super.writeEntityToNBT(compound);
+        compound.setInteger("Variant", this.getVariant());
+    }
+
+    @Override
+    public void readEntityFromNBT(NBTTagCompound compound) {
+        super.readEntityFromNBT(compound);
+        if (compound.hasKey("Variant")) {
+            this.setVariant(compound.getInteger("Variant"));
+        }
     }
 
     @Override
