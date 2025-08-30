cd9146237474c090349479620b11411a911fb0c4
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-30 23:18:25 +0200

    fix: change default creative tab

--- src/main/java/de/julianweinelt/ubm/blocks/interactable/BlockSmithingTable.java
+++ src/main/java/de/julianweinelt/ubm/blocks/interactable/BlockSmithingTable.java
@@ -1,6 +1,7 @@
 package de.julianweinelt.ubm.blocks.interactable;
 
 import de.julianweinelt.ubm.blocks.interactable.smithing.TileEntitySmithingTable;
+import de.julianweinelt.ubm.misc.ModCreativeTabs;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
@@ -13,6 +14,7 @@ public class BlockSmithingTable extends BlockContainer {
         super(Material.WOOD);
         setUnlocalizedName("smithing_table");
         setRegistryName("smithing_table");
+        setCreativeTab(ModCreativeTabs.UBM_TAB_PILLAGE);
     }
 
     @Nullable
