ab26b9c87459bf82060748357563855389c098f5
Julian Weinelt <julian.weinelt@outlook.de>
2025-08-30 17:25:45 +0200

    feat: add glow squid

---
+++ src/main/java/de/julianweinelt/ubm/entities/EntityGlowSquid.java
@@ -0,0 +1,10 @@
+package de.julianweinelt.ubm.entities;
+
+import net.minecraft.entity.passive.EntitySquid;
+import net.minecraft.world.World;
+
+public class EntityGlowSquid extends EntitySquid {
+    public EntityGlowSquid(World worldIn) {
+        super(worldIn);
+    }
+}
