610c24a4514b81f757d0ea43a308c1faf5eba15d
Julian Weinelt <julian.weinelt@outlook.de>
2025-09-19 15:50:23 +0200

    api: add base item for armor trims

---
+++ src/main/java/de/julianweinelt/ubm/items/ItemArmorTrim.java
@@ -0,0 +1,7 @@
+package de.julianweinelt.ubm.items;
+
+import de.julianweinelt.ubm.blocks.interactable.smithing.IArmorTrim;
+import net.minecraft.item.Item;
+
+public class ItemArmorTrim extends Item implements IArmorTrim {
+}
