2f12e65a4f74e973dacd2a6a2fe538f490cb59b0
Julian Weinelt <julian.weinelt@outlook.de>
2025-09-20 20:26:34 +0200

    feat: add custom main menu screen (WIP)

--- src/main/java/de/julianweinelt/ubm/misc/ClientEventHandler.java
+++ src/main/java/de/julianweinelt/ubm/misc/ClientEventHandler.java
@@ -2,8 +2,12 @@ package de.julianweinelt.ubm.misc;
 
 import de.julianweinelt.ubm.UBM;
 import de.julianweinelt.ubm.mixin.GuiChatWithSuggestions;
+import de.julianweinelt.ubm.mixin.GuiMainMenuNonBlur;
 import de.julianweinelt.ubm.particles.ParticleCopperFlameFactory;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
@@ -23,6 +27,16 @@ public class ClientEventHandler {
         if (Keyboard.getEventKeyState() && Keyboard.getEventKey() == Keyboard.KEY_U) {
             Minecraft.getMinecraft().displayGuiScreen(new GuiChatWithSuggestions());
             UBM.getLogger().info("Opening chat gui");
+        }
+    }
+
+    @SubscribeEvent
+    public static void onGuiOpen(GuiOpenEvent event) {
+        if (event.getGui() instanceof GuiChat) {
+            event.setGui(new GuiChatWithSuggestions());
+        }
+        if (event.getGui() instanceof GuiMainMenu) {
+            //event.setGui(new GuiMainMenuNonBlur());
         }
     }
 
