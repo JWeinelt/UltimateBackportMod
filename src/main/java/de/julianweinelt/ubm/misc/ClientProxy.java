93fac9969cc83f65d2a809db0572fe48564a75a5
Julian Weinelt <julian.weinelt@outlook.de>
2025-09-20 19:03:24 +0200

    feat: display armor trims on armor stands, zombies and skeletons

--- src/main/java/de/julianweinelt/ubm/misc/ClientProxy.java
+++ src/main/java/de/julianweinelt/ubm/misc/ClientProxy.java
@@ -1,18 +1,29 @@
 package de.julianweinelt.ubm.misc;
 
+import de.julianweinelt.ubm.blocks.tiles.RenderTileEntityCampfire;
+import de.julianweinelt.ubm.blocks.tiles.TileEntityCampfire;
 import de.julianweinelt.ubm.items.ModItems;
 import de.julianweinelt.ubm.trims.LayerArmorTrim;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Map;
 
 @SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
@@ -24,10 +35,32 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void init(FMLInitializationEvent e) {
+        Map<Class<? extends Entity>, Render<? extends Entity>> renderMap =
+                Minecraft.getMinecraft().getRenderManager().entityRenderMap;
+
+        addLayerIfLiving(renderMap.get(EntityZombie.class));
+        addLayerIfLiving(renderMap.get(EntitySkeleton.class));
+        addLayerIfLiving(renderMap.get(EntityArmorStand.class));
+
         super.init(e);
 
         for (RenderPlayer renderer : Minecraft.getMinecraft().getRenderManager().getSkinMap().values()) {
             renderer.addLayer(new LayerArmorTrim(renderer));
         }
+
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityCampfire.class, new RenderTileEntityCampfire());
+    }
+
+    private static void addLayerIfLiving(Render<? extends Entity> renderer) {
+        if(renderer instanceof RenderLivingBase) {
+            ((RenderLivingBase<?>) renderer).addLayer(new LayerArmorTrim((RenderLivingBase<?>) renderer));
+        }
+    }
+
+
+
+    @SubscribeEvent
+    public static void onModelRegistry(ModelRegistryEvent event) {
+        ModModels.registerAllArmorModels();
     }
 }
