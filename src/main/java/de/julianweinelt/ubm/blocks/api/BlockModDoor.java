f6a477353da5d17bdd8c495f20e7944a187fc8da
Julian Weinelt <julian.weinelt@outlook.de>
2025-09-30 13:47:03 +0200

    fix: doors can't have a transparent texture

--- src/main/java/de/julianweinelt/ubm/blocks/api/BlockModDoor.java
+++ src/main/java/de/julianweinelt/ubm/blocks/api/BlockModDoor.java
@@ -2,11 +2,38 @@ package de.julianweinelt.ubm.blocks.api;
 
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockModDoor extends BlockDoor {
     protected BlockModDoor(Material materialIn, String name) {
         super(materialIn);
         this.setUnlocalizedName(name);
         this.setRegistryName(name);
+    }
+
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+    @Override
+    public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        return true;
     }
 }
