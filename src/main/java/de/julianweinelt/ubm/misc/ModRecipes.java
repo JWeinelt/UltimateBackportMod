5fd40f97d1c0c675d40fb888cf1625276eff85bb
Julian Weinelt <julian.weinelt@outlook.de>
2025-09-30 10:44:15 +0200

    feat: add smelting recipes for ores

--- src/main/java/de/julianweinelt/ubm/misc/ModRecipes.java
+++ src/main/java/de/julianweinelt/ubm/misc/ModRecipes.java
@@ -11,6 +11,17 @@ public class ModRecipes {
         GameRegistry.addSmelting(ModItems.RAW_GOLD, new ItemStack(Items.GOLD_INGOT, 1), 0.7F);
         GameRegistry.addSmelting(ModItems.RAW_IRON, new ItemStack(Items.IRON_INGOT, 1), 0.7F);
         GameRegistry.addSmelting(ModItems.RAW_COPPER, new ItemStack(ModItems.COPPER_INGOT, 1), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.COPPER_ORE, new ItemStack(ModItems.COPPER_INGOT, 1), 0.7F);
+
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_COPPER, new ItemStack(ModItems.COPPER_INGOT, 1), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_LAPISLAZULI, new ItemStack(Items.DYE, 1, 4), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_GOLD, new ItemStack(Items.GOLD_INGOT, 1), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_REDSTONE, new ItemStack(Items.REDSTONE, 1), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_EMERALD, new ItemStack(Items.EMERALD, 1), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_DIAMOND, new ItemStack(Items.DIAMOND, 1), 0.7F);
+        GameRegistry.addSmelting(ModBlocks.DEEPSLATE_ORE_IRON, new ItemStack(Items.IRON_INGOT, 1), 0.7F);
+
+
         GameRegistry.addSmelting(ModBlocks.ANCIENT_DEBRIS, new ItemStack(ModItems.NETHERITE_SCRAP, 1), 0.7F);
 
     }
