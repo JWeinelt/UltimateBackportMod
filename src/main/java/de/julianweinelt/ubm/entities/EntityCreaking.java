42b9f266c42c4c1410c4b38810e3677a141eaff5
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-01 10:47:31 +0200

    feat(entities): add creaking

---
+++ src/main/java/de/julianweinelt/ubm/entities/EntityCreaking.java
@@ -0,0 +1,10 @@
+package de.julianweinelt.ubm.entities;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+public class EntityCreaking extends EntityMob {
+    public EntityCreaking(World worldIn) {
+        super(worldIn);
+    }
+}
