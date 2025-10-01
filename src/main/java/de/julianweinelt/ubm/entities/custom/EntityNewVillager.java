808fbc4949e3f28e1c07a066b32ac24877ef877d
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-01 10:49:47 +0200

    api: change villager type

--- src/main/java/de/julianweinelt/ubm/entities/custom/EntityNewVillager.java
+++ src/main/java/de/julianweinelt/ubm/entities/custom/EntityNewVillager.java
@@ -1,9 +1,19 @@
 package de.julianweinelt.ubm.entities.custom;
 
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.IMerchant;
+import net.minecraft.entity.INpc;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.MerchantRecipe;
+import net.minecraft.village.MerchantRecipeList;
 import net.minecraft.world.World;
 
-public class EntityNewVillager extends EntityVillager {
+import javax.annotation.Nullable;
+
+public class EntityNewVillager extends EntityCreature implements INpc {
     public EntityNewVillager(World worldIn) {
         super(worldIn);
     }
