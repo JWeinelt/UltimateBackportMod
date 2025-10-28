1d13f693529e852ba5351922d349c3240a27b10f
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-28 15:46:38 +0100

    fix: make allay leachable

--- src/main/java/de/julianweinelt/ubm/entities/EntityAllay.java
+++ src/main/java/de/julianweinelt/ubm/entities/EntityAllay.java
@@ -1,15 +1,18 @@
 package de.julianweinelt.ubm.entities;
 
-import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityFlying;
-import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public class EntityAllay extends EntityFlying {
     public EntityAllay(World worldIn) {
         super(worldIn);
+    }
+
+
+
+    @Override
+    public boolean canBeLeashedTo(EntityPlayer player) {
+        return true;
     }
 }
