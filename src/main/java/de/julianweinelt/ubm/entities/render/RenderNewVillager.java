5a3c8381de6cd3c8b801f5e2d0a4b28f919731e1
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-28 16:18:36 +0100

    improvement: render villager babies in the right size

--- src/main/java/de/julianweinelt/ubm/entities/render/RenderNewVillager.java
+++ src/main/java/de/julianweinelt/ubm/entities/render/RenderNewVillager.java
@@ -2,6 +2,7 @@ package de.julianweinelt.ubm.entities.render;
 
 import de.julianweinelt.ubm.entities.custom.EntityNewVillager;
 import de.julianweinelt.ubm.entities.models.ModelVillager;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -22,5 +23,12 @@ public class RenderNewVillager extends RenderLiving<EntityNewVillager> {
     @Nonnull
     protected ResourceLocation getEntityTexture(@Nullable EntityNewVillager entity) {
         return new ResourceLocation("ubm:textures/entity/villager/villager.png");
+    }
+
+    @Override
+    protected void preRenderCallback(@Nullable EntityNewVillager en, float partialTickTime) {
+        if (en == null) return;
+        float scale = en.isChild() ? 0.5F : 1.0F;
+        GlStateManager.scale(scale, scale, scale);
     }
 }
