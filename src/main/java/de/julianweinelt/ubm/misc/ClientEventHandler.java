6174d9d6596c50b8b2ce520c2dd5d13a4aba10a2
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-28 17:12:03 +0100

    cleanup

--- src/main/java/de/julianweinelt/ubm/misc/ClientEventHandler.java
+++ src/main/java/de/julianweinelt/ubm/misc/ClientEventHandler.java
@@ -2,19 +2,16 @@ package de.julianweinelt.ubm.misc;
 
 import de.julianweinelt.ubm.UBM;
 import de.julianweinelt.ubm.mixin.GuiChatWithSuggestions;
-import de.julianweinelt.ubm.mixin.GuiMainMenuNonBlur;
 import de.julianweinelt.ubm.particles.ParticleCopperFlameFactory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.input.Keyboard;
 
 @Mod.EventBusSubscriber(modid = UBM.MODID)
 public class ClientEventHandler {
