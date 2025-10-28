8e7c70a9a362d264b51f745c996491b4e3cb19ae
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-28 15:46:49 +0100

    feat: add AI to armadillo

--- src/main/java/de/julianweinelt/ubm/entities/EntityArmadillo.java
+++ src/main/java/de/julianweinelt/ubm/entities/EntityArmadillo.java
@@ -1,7 +1,13 @@
 package de.julianweinelt.ubm.entities;
 
+import de.julianweinelt.ubm.configuration.ModConfig;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
@@ -10,6 +16,7 @@ import javax.annotation.Nullable;
 public class EntityArmadillo extends EntityAnimal {
     public EntityArmadillo(World worldIn) {
         super(worldIn);
+        this.setSize(0.7F, 0.65F);
     }
 
     @Nullable
@@ -19,4 +26,29 @@ public class EntityArmadillo extends EntityAnimal {
         child.setGrowingAge(-24000);
         return child;
     }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(6);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.14);
+    }
+
+    @Override
+    public boolean isBreedingItem(ItemStack stack) {
+        return stack.getItem() == Items.SPIDER_EYE;
+    }
+
+    @Override
+    public boolean canBeLeashedTo(@Nonnull EntityPlayer player) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    protected Item getDropItem() {
+        return Items.SPIDER_EYE; //TODO: Add armadillo scute as drop
+    }
+
+
 }
