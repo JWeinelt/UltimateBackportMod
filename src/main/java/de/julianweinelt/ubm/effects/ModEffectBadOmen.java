e9709f453f79b0140e28d4d7e5c72dc53e2ed78c
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-31 15:22:47 +0100

    feat: add status effect "bad omen"

---
+++ src/main/java/de/julianweinelt/ubm/effects/ModEffectBadOmen.java
@@ -0,0 +1,9 @@
+package de.julianweinelt.ubm.effects;
+
+import net.minecraft.potion.Potion;
+
+public class ModEffectBadOmen extends Potion {
+    protected ModEffectBadOmen() {
+        super(true, 0x0b6138);
+    }
+}
