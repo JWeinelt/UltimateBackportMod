45ef08e741394aa7c27c37cf52e100f25d449182
Julian Weinelt <julian.weinelt@outlook.de>
2025-10-31 15:22:58 +0100

    feat: add status effect "hero of the village"

---
+++ src/main/java/de/julianweinelt/ubm/effects/ModEffectHeroOfTheVillage.java
@@ -0,0 +1,9 @@
+package de.julianweinelt.ubm.effects;
+
+import net.minecraft.potion.Potion;
+
+public class ModEffectHeroOfTheVillage extends Potion {
+    protected ModEffectHeroOfTheVillage() {
+        super(false, 0x000000);
+    }
+}
