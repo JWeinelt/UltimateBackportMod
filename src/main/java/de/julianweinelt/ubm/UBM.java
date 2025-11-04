bcfbfb07593d04208286e2c3d324b0090e1ed102
Julian Weinelt <julian.weinelt@outlook.de>
2025-11-04 19:29:14 +0100

    update version strings

--- src/main/java/de/julianweinelt/ubm/UBM.java
+++ src/main/java/de/julianweinelt/ubm/UBM.java
@@ -33,7 +33,7 @@ import java.io.InputStream;
 public class UBM {
     public static final String MODID = "ubm";
     public static final String NAME = "Ultimate Backport Mod";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
 
     private static Logger logger;
     public static UBM instance;
